3ee34116ccd53c0cb56553cb852a33486f8b445a
chorng <chung.horng@sinergise.com>
2024-04-15 03:44:52 +0200

    Update evalscript
    * add cloud mask and water mask
    
    * apply usgs dnbr general classification

--- scripts/evalscripts.py
+++ scripts/evalscripts.py
@@ -1,21 +1,26 @@
-def burn_severity_visualisation(fire_start, fire_end, moderate_burned_threshold, severe_burned_threshold):
+def burn_severity_visualisation(fire_start, fire_end):
     return f"""
         //VERSION=3
         function setup() {{
             return {{
-                input: [{{bands: ["B02", "B03", "B04", "B05", "B08", "B12"]}}],
+                input: [{{bands: ["B02", "B03", "B04", "B05", "B08", "B12", "SCL]}}],
                 output: {{ bands: 3 }},
                 mosaicking: "ORBIT"
             }}
           }}
 
-        function stretch(val, min, max) {{return (val - min) / (max - min);}}
-
         function calcNBR(nir, swir) {{
-            var denom = nir + swir;
-            var nbrval = denom != 0 ? (nir - swir) / denom : NaN;
+            const denom = nir + swir;
+            const nbrval = denom != 0 ? (nir - swir) / denom : NaN;
             return nbrval;
         }}
+
+        const severity_map = [
+            [0.27, 0xfecc5c],
+            [0.44, 0xfd8d3c],
+            [0.66, 0xe31a1c],
+        ];
+        const severity_visualizer = new ColorMapVisualizer(severity_map);
 
         function evaluatePixel(samples) {{
             // calculate delta nbr
@@ -23,27 +28,18 @@ def burn_severity_visualisation(fire_start, fire_end, moderate_burned_threshold,
             const nbrpost = calcNBR(samples[1].B08, samples[1].B12);
             const dnbr = nbrpre - nbrpost;
 
+            // mask cloud and water
+            const cloud_and_water = [3, 6, 7, 8, 9, 10];
+            const is_cloud_or_water = (
+                cloud_and_water.includes(samples[0].SCL) ||
+                cloud_and_water.includes(samples[1].SCL);
+            );
+
             // set output display layers
-            const stretchMin = 0.05;
-            const stretchMax = 1.00;
-            const naturalColors = [
-                stretch(2.8 * samples[0].B04 + 0.1 * samples[0].B05, stretchMin, stretchMax),
-                stretch(2.8 * samples[0].B03 + 0.15 * samples[0].B08, stretchMin, stretchMax),
-                stretch(2.8 * samples[0].B02, stretchMin, stretchMax)
-            ];
-            const burnModerate = [
-                stretch(2.8 * samples[0].B04 + 0.1 * samples[0].B05, stretchMin,stretchMax)+0.5,
-                stretch(2.8 * samples[0].B03 + 0.15 * samples[0].B08, stretchMin, stretchMax)+0.5,
-                stretch(2.8 * samples[0].B02, stretchMin, stretchMax)
-            ];
-            const burnSevere = [
-                stretch(2.8 * samples[0].B04 + 0.1 * samples[0].B05, stretchMin, stretchMax)+0.5,
-                stretch(2.8 * samples[0].B03 + 0.15 * samples[0].B08, stretchMin, stretchMax),
-                stretch(2.8 * samples[0].B02, stretchMin, stretchMax)
-            ];
-            return (dnbr < {moderate_burned_threshold} || isNaN(dnbr)) ? naturalColors
-                : (dnbr < {severe_burned_threshold}) ? burnModerate
-                : burnSevere;
+            const natural_color = [2.5 * samples[0].B04, 2.5 * samples[0].B03, 2.5 * samples[0].B02];
+            let img_vals = dnbr >= 0.27 ? severity_visualizer.process(dnbr) : natural_color
+            img_vals = is_cloud_or_water ? natural_color : img_vals;
+            return img_vals
         }}
 
         function preProcessScenes (collections) {{
@@ -53,8 +49,8 @@ def burn_severity_visualisation(fire_start, fire_end, moderate_burned_threshold,
             collections.scenes.orbits = collections.scenes.orbits.sort(function(a, b) {{
                 return new Date(a.dateFrom) - new Date(b.dateFrom);
             }})
-            var preFireStartScene = collections.scenes.orbits[0].dateFrom;
-            var postFireEndScene = collections.scenes.orbits[numScenes-1].dateFrom;
+            let preFireStartScene = collections.scenes.orbits[0].dateFrom;
+            let postFireEndScene = collections.scenes.orbits[numScenes-1].dateFrom;
 
             // get the nearest scene before the fire
             for (let i = 0; i < numScenes; i++) {{
@@ -75,9 +71,9 @@ def burn_severity_visualisation(fire_start, fire_end, moderate_burned_threshold,
                   postFireEndScene = currentDate;
               }}
             }}
-            var allowedDates = [preFireStartScene, postFireEndScene];
+            let allowedDates = [preFireStartScene, postFireEndScene];
             collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {{
-                var orbitDateFrom = orbit.dateFrom;
+                let orbitDateFrom = orbit.dateFrom;
                 return allowedDates.includes(orbitDateFrom);
             }})
             return collections
