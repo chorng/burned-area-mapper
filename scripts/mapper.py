6d469ddcca3c26beea778bc04e381ca85743d024
chorng <chung.horng@sinergise.com>
2024-04-16 23:26:19 +0200

    Implement large area utilities

--- scripts/mapper.py
+++ scripts/mapper.py
@@ -3,14 +3,26 @@ from datetime import date, timedelta
 from pathlib import Path
 
 from evalscripts import burn_severity_visualisation
-from sentinelhub import CRS, BBox, DataCollection, MimeType, SentinelHubRequest, SHConfig, bbox_to_dimensions
+from sentinelhub import (
+    CRS,
+    BBox,
+    DataCollection,
+    MimeType,
+    SentinelHubDownloadClient,
+    SentinelHubRequest,
+    SHConfig,
+    UtmZoneSplitter,
+)
 
 # input settings
-AOI = (149.390717, -33.016725, 149.637909, -32.844981)
+# AOI = (149.390717, -33.016725, 149.637909, -32.844981)
+AOI = (148.79697, -33.20518, 150.05036, -32.64876)
 FIRE_START = "2023-03-05"
 FIRE_END = "2023-03-19"
 TIME_DELTA = timedelta(days=10)
-RESULTS_DIR = Path(__file__).parent / ".." / "results"
+RESOLUTION = 10
+MAXCC = 0.3
+RESULT_DIR = Path(__file__).parent / ".." / "results"
 
 # configurations
 config = SHConfig()
@@ -19,28 +31,39 @@ config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE
 config.sh_client_id = os.environ["SH_CLIENT_ID"]
 config.sh_client_secret = os.environ["SH_CLIENT_SECRET"]
 
-# compile requests
+# split aoi
 bbox = BBox(AOI, crs=CRS.WGS84)
+utm_zone_splitter = UtmZoneSplitter([bbox.geometry], CRS.WGS84, (20000, 20000))
+bbox_list = utm_zone_splitter.get_bbox_list()
+
+
+# compile requests
+def burned_area_map_request(bbox, time_interval, resolution, maxcc, evalscript, result_dir, config):
+    return SentinelHubRequest(
+        data_folder=result_dir,
+        evalscript=evalscript,
+        input_data=[
+            SentinelHubRequest.input_data(
+                data_collection=DataCollection.SENTINEL2_L2A.define_from("CDSE_S2L2A", service_url=config.sh_base_url),
+                time_interval=time_interval,
+                maxcc=maxcc,
+            )
+        ],
+        responses=[SentinelHubRequest.output_response("default", MimeType.TIFF)],
+        bbox=bbox,
+        resolution=tuple([resolution] * 2),
+        config=config,
+    )
+
+
 time_interval = (
     (date.fromisoformat(FIRE_START) - TIME_DELTA).strftime("%Y-%m-%d"),
     (date.fromisoformat(FIRE_END) + TIME_DELTA).strftime("%Y-%m-%d"),
 )
-size = bbox_to_dimensions(bbox, resolution=10)
 evalscript = burn_severity_visualisation(FIRE_START, FIRE_END)
-request = SentinelHubRequest(
-    data_folder=RESULTS_DIR,
-    evalscript=evalscript,
-    input_data=[
-        SentinelHubRequest.input_data(
-            data_collection=DataCollection.SENTINEL2_L2A.define_from("CDSE_S2L2A", service_url=config.sh_base_url),
-            time_interval=time_interval,
-        )
-    ],
-    responses=[SentinelHubRequest.output_response("default", MimeType.TIFF)],
-    bbox=bbox,
-    size=size,
-    config=config,
-)
-
-# download maps
-response = request.get_data(save_data=True)
+requests = [
+    burned_area_map_request(bbox, time_interval, RESOLUTION, MAXCC, evalscript, RESULT_DIR, config)
+    for bbox in bbox_list
+]
+download_requests = [request.download_list[0] for request in requests]
+downloaded_data = SentinelHubDownloadClient(config=config).download(download_requests, max_threads=10)
--- tests/test_evalscripts.py
+++ tests/test_evalscripts.py
@@ -1,19 +1,26 @@
-def burn_severity_visualisation(fire_start, fire_end):
-    return f"""
-        //VERSION=3
-        function setup() {{
-            return {{
-                input: [{{bands: ["B02", "B03", "B04", "B08", "B12", "SCL"]}}],
-                output: {{ bands: 3 }},
-                mosaicking: "ORBIT"
-            }}
-        }}
+import pytest
 
-        function calcNBR(nir, swir) {{
+from scripts.evalscripts import burn_severity_visualisation
+
+
+@pytest.mark.parametrize("kwargs", [dict(fire_start="2023-03-05", fire_end="2023-03-19")])
+def test_burn_severity_visualisation(kwargs) -> None:
+    evalscript = burn_severity_visualisation(**kwargs)
+    expected_evalscript = """
+        //VERSION=3
+        function setup() {
+            return {
+                input: [{bands: ["B02", "B03", "B04", "B08", "B12", "SCL"]}],
+                output: { bands: 3 },
+                mosaicking: "ORBIT"
+            }
+        }
+
+        function calcNBR(nir, swir) {
             const denom = nir + swir;
             const nbrval = denom != 0 ? (nir - swir) / denom : NaN;
             return nbrval;
-        }}
+        }
 
         // usgs dnbr general classification
         const severity_map = [
@@ -23,7 +30,7 @@ def burn_severity_visualisation(fire_start, fire_end):
         ];
         const severity_visualizer = new ColorMapVisualizer(severity_map);
 
-        function evaluatePixel(samples) {{
+        function evaluatePixel(samples) {
             // calculate delta nbr
             const nbrpre = calcNBR(samples[0].B08, samples[0].B12);
             const nbrpost = calcNBR(samples[1].B08, samples[1].B12);
@@ -41,42 +48,43 @@ def burn_severity_visualisation(fire_start, fire_end):
             let img_vals = dnbr >= 0.27 ? severity_visualizer.process(dnbr) : natural_color
             img_vals = is_cloud_or_water ? natural_color : img_vals;
             return img_vals
-        }}
+        }
 
-        function preProcessScenes (collections) {{
-            const fireStart = "{fire_start}";
-            const fireEnd = "{fire_end}";
+        function preProcessScenes (collections) {
+            const fireStart = "2023-03-05";
+            const fireEnd = "2023-03-19";
             const numScenes = collections.scenes.orbits.length;
-            collections.scenes.orbits = collections.scenes.orbits.sort(function(a, b) {{
+            collections.scenes.orbits = collections.scenes.orbits.sort(function(a, b) {
                 return new Date(a.dateFrom) - new Date(b.dateFrom);
-            }})
+            })
             let preFireStartScene = collections.scenes.orbits[0].dateFrom;
             let postFireEndScene = collections.scenes.orbits[numScenes-1].dateFrom;
 
             // get the nearest scene before the fire
-            for (let i = 0; i < numScenes; i++) {{
+            for (let i = 0; i < numScenes; i++) {
                 currentDate = collections.scenes.orbits[i].dateFrom;
                 if (new Date(currentDate) > new Date(preFireStartScene) &&
                     new Date(currentDate) <= new Date(fireStart)
-                ) {{
+                ) {
                     preFireStartScene = currentDate;
-                }}
-            }}
+                }
+            }
 
             // get the nearest scene after the fire
-            for (let i = numScenes - 1; i > -1; i--) {{
+            for (let i = numScenes - 1; i > -1; i--) {
                 currentDate = collections.scenes.orbits[i].dateFrom;
                 if (new Date(currentDate) < new Date(postFireEndScene) &&
                     new Date(currentDate) >= new Date(fireEnd)
-                ) {{
+                ) {
                     postFireEndScene = currentDate;
-                }}
-            }}
+                }
+            }
             const allowedDates = [preFireStartScene, postFireEndScene];
-            collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {{
+            collections.scenes.orbits = collections.scenes.orbits.filter(function (orbit) {
                 let orbitDateFrom = orbit.dateFrom;
                 return allowedDates.includes(orbitDateFrom);
-            }})
+            })
             return collections
-        }}
+        }
     """
+    assert evalscript == expected_evalscript, "Wrong evalscript"
