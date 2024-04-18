43374c88b300ca0be9cc3bc5bb2c7b4377dd6555
chorng <chung.horng@sinergise.com>
2024-04-18 19:25:12 +0200

    Add tests for BurnedAreaMapper class

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,5 +1,10 @@
 from datetime import date
 
+import pytest
+from sentinelhub import MimeType
+
+from src.evalscripts import burn_severity_visualisation, burned_area_mask
+
 
 def test_time_interval(mapper_fixture):
     assert date.fromisoformat(mapper_fixture.time_interval[0]) < date.fromisoformat(
@@ -19,3 +24,22 @@ def test_token_endpoint(mapper_fixture):
         mapper_fixture.config.sh_token_url
         == "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
     ), "Wrong token endpint"
+
+
+@pytest.mark.parametrize(("bbox_size", "expected_num_bbox"), [(20000, 36), (60000, 8)])
+def test_split_into_utm_zones(mapper_fixture, bbox_size, expected_num_bbox):
+    bbox_list = mapper_fixture.split_into_utm_zones(bbox_size)
+    assert len(bbox_list) == expected_num_bbox, "Wrong number of bbox"
+
+
+@pytest.mark.parametrize(
+    ("bbox_size", "evalscript_func", "output_format", "expected_num_request"),
+    [
+        (20000, burn_severity_visualisation, MimeType.PNG, 36),
+        (60000, burned_area_mask, MimeType.TIFF, 8),
+    ],
+)
+def test_create_request_list(mapper_fixture, bbox_size, evalscript_func, output_format, expected_num_request):
+    mapper_fixture.split_into_utm_zones(bbox_size)
+    request_list = mapper_fixture.create_request_list(evalscript_func, output_format)
+    assert len(request_list) == expected_num_request, "Wrong number of request"
